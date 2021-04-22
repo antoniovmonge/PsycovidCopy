b4a0ca9e108ef04deaabcc4451fd1ce1b588699f
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2021-04-22 12:02:02 +0200

    leave this version

--- app2.py
+++ app2.py
@@ -17,15 +17,15 @@ def app():
     st.sidebar.title('Visualisation Selector')
 
     # Comment/UNCOMMENT THOSE LINES TO ACTIVTE GCP PATH
-    storage_client = storage.Client()
+    client = storage.Client()
     
     path = f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}"
     
     @st.cache
     def get_cached_data():
-        return pd.read_csv(path).drop(columns='Unnamed: 0')
+        df = pd.read_csv(path).drop(columns='Unnamed: 0')
+        return df
     
-    df = get_cached_data() 
     # df = pd.read_csv(
     #     'raw_data/cleaned_data_040321.csv').drop(columns='Unnamed: 0')
 
