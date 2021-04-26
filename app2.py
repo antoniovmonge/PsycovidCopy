d764312f1b9e505b0e44335f1341b06366ac7f1d
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2021-04-26 13:14:48 +0200

    updated app2

--- app2.py
+++ app2.py
@@ -6,6 +6,8 @@ import seaborn as sns
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly.express as px
+import matplotlib.ticker as mtick
+
 # from google.cloud import storage
 # from psycovid.params import *
 # import os
@@ -16,7 +18,7 @@ import plotly.express as px
 #     "/home/antoniovmonge/code/antoniovmonge/gcp_keys/psycovid-beta-6cfec8fe1775.json")
 def app():
 
-    st.sidebar.title('Visualisation Selector')
+    # st.title('Visualisation Selector')
 
     # Comment/UNCOMMENT THOSE LINES TO ACTIVTE GCP PATH
     # client = language.LanguageServiceClient(credentials=credentials)
@@ -33,78 +35,25 @@ def app():
     #     'raw_data/cleaned_data_040321.csv').drop(columns='Unnamed: 0')
 
 
-    #select = st.sidebar.selectbox('Select a State',df['state'])
-
-    st.sidebar.markdown('Personality traits accross countries')
-    user_select = st.sidebar.multiselect(
-        'Select countries', df['Country'].unique())
-
-    #get the country selected in the selectbox
-    select_country = df.loc[df['Country'].isin(user_select)]
-
-
-    def country_stats():
-
-        # Define variables
-        Country_neu = select_country.groupby('Country')['neu'].mean()
-        Country_ext = select_country.groupby('Country')['ext'].mean()
-        Country_ope = select_country.groupby('Country')['ope'].mean()
-        Country_con = select_country.groupby('Country')['con'].mean()
-        Country_agr = select_country.groupby('Country')['agr'].mean()
-
-        fig = make_subplots(rows=5, cols=1, shared_xaxes=True)
-
-        fig.append_trace(go.Bar(x=Country_neu.index,
-                                y=Country_neu.values), row=1, col=1)
-        fig.update_yaxes(range=(0, 5), title_text="Neuroticism", row=1, col=1)
-
-        fig.append_trace(go.Bar(x=Country_ope.index,
-                                y=Country_ope.values), row=2, col=1)
-        fig.update_yaxes(range=(0, 5), title_text="Openness", row=2, col=1)
-
-        fig.append_trace(go.Bar(x=Country_ext.index,
-                                y=Country_ext.values), row=3, col=1)
-        fig.update_yaxes(range=(0, 5), title_text="Extraversion", row=3, col=1)
-
-        fig.append_trace(go.Bar(x=Country_agr.index,
-                                y=Country_agr.values), row=4, col=1)
-        fig.update_yaxes(range=(0, 5), title_text="Agreeableness", row=4, col=1)
-
-        fig.append_trace(go.Bar(x=Country_con.index,
-                                y=Country_con.values), row=5, col=1)
-        fig.update_yaxes(
-            range=(0, 5), title_text="Conscientiousness", row=5, col=1)
-
-        fig.update_layout(height=800, width=600, title_text="Personality traits per country",
-                        xaxis_tickangle=90, showlegend=False)
-
-        return fig
-
     # PLOTLY RADAR CHART
     def country_radar():
-        Country_neu = select_country.groupby('Country')['neu'].mean()
-        Country_ext = select_country.groupby('Country')['ext'].mean()
-        Country_ope = select_country.groupby('Country')['ope'].mean()
-        Country_con = select_country.groupby('Country')['con'].mean()
-        Country_agr = select_country.groupby('Country')['agr'].mean()
-        
         categories = ['Neuroticism', 'Openness', 'Extraversion',
-                      'Agreeableness', 'Conscientiousness']
-        
+                      'Agreeableness', 'Conscientiousness', 'Neuroticism']
+
+
         fig = go.Figure()
 
-        fig.add_trace(go.Scatterpolar(
-            r=[Country_neu, 5, 2, 2, 3],
-            theta=categories,
-            fill='toself',
-            name='Product A'
-        ))
-        # fig.add_trace(go.Scatterpolar(
-        #     r=[4, 3, 2.5, 1, 2],
-        #     theta=categories,
-        #     fill='toself',
-        #     name='Product B'
-        # ))
+        for i in user_select:
+            fig.add_trace(go.Scatterpolar(
+                r=[df[df.Country == i].groupby('Country')['neu'].mean().to_list()[0],
+                   df[df.Country == i].groupby('Country')['ope'].mean().to_list()[0],
+                   df[df.Country == i].groupby('Country')['ext'].mean().to_list()[0],
+                   df[df.Country == i].groupby('Country')['agr'].mean().to_list()[0],
+                   df[df.Country == i].groupby('Country')['con'].mean().to_list()[0],
+                   df[df.Country == i].groupby('Country')['neu'].mean().to_list()[0]],
+                theta=categories,
+                fill='toself',
+                name=i))
 
         fig.update_layout(
             polar=dict(
@@ -112,11 +61,10 @@ def app():
                     visible=True,
                     range=[0, 5]
                 )),
-            showlegend=False
+            showlegend=True
         )
 
         # fig.show()
-        
         return fig
     
     def stress():
@@ -126,9 +74,9 @@ def app():
         for i in items:
             sns.kdeplot(data=df[df['Country'] == i],
                         x="PSS10_avg", common_norm=False, label=i)
-        plt.title('STRESS', fontsize=20)
-        plt.xlabel('Perceived Stress', size=15)
-        plt.ylabel('Number of people', size=15)
+        plt.title('STRESS')
+        plt.xlabel('Perceived Stress')
+        plt.ylabel('Distribution')
         plt.legend()
 
         return fig
@@ -141,12 +89,12 @@ def app():
         
         for i in user_select:
             items.append(i)
-        
+            # sns.set_style("talk")
             sns.kdeplot(data=df[df['Country'] == i],
                         x="SLON3_avg", common_norm=False, label=i)
-            plt.title('LONLINESS', fontsize=20)
-            plt.xlabel('Percived Loneliness', size=15)
-            plt.ylabel('Percentage of people', size=15)
+            plt.title('LONLINESS')
+            plt.xlabel('Percived Loneliness')
+            plt.ylabel('Distribution')
             plt.legend()
 
         return fig
@@ -154,22 +102,26 @@ def app():
     # Create Columns
     # col1, col2 = st.beta_columns(2)
 
+    st.markdown('## Personality traits accross countries')
+    user_select = st.multiselect(
+        'Select countries', df['Country'].unique())
 
+    #get the country selected in the selectbox
+    sns.set_style('white')
+    # select_country = df.loc[df['Country'].isin(user_select)]
     items = []
     for i in user_select:
         items.append(i)
-    if st.sidebar.button('Apply'):
+    if st.button('Apply'):
         # print is visible in server output, not in the page
         print('button clicked!')
-        st.write(country_radar())
-        st.plotly_chart(country_stats(), use_container_width=False)
-        col1, col2 = st.beta_columns(2)
-        col1.write(stress(), use_column_width=True)
-        col2.write(loneliness(), use_column_width=True)
+        
+        col1, col2, col3 = st.beta_columns((1.5,1,1))
+        col1.plotly_chart(country_radar(), use_container_width=True)
+        # col1.plotly_chart(country_stats(), use_container_width=True)
+        
+        col2.pyplot(stress())
+        col2.pyplot(loneliness())
         items = []
     else:
-        st.sidebar.write('Press to apply')
-        st.title('Please, select countries to compare')
-        st.header('...and press Apply')
-    
-    
+        st.write('Press to apply')
